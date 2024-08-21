9c71beb25932873a191831cd575e610802989aac
antuneseds <esterdossantosantunes04@gmail.com>
2024-08-21 11:52:55 -0300

    feat: melhoria rodapé #2

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,24 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import FooterInfo from "./components/FooterInfo.vue"
 </script>
 
 <template>
-  <footer class="footer">Rodapé</footer>  
-
+  <footer>
+    <footer-info />
+  </footer>  
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+footer {
+  position: fixed;
+  bottom: 0;
+  background-color: #d9d9d9;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/components/FooterInfo.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+   RODAPÉ (localizão, redes sociais, telefone, email, etc)
+</template>
+
+<style scoped>
+
+</style>
