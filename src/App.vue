a2e50f6476b1828861351b3cd66e9bcf041bed7d
antuneseds <esterdossantosantunes04@gmail.com>
2024-08-23 11:46:12 -0300

    feat: melhorias do rodapé #2

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,10 @@ import FooterInfo from "./components/FooterInfo.vue"
 </script>
 
 <template>
-  <footer>
-    <footer-info />
-  </footer>  
+  
+  <footer-info />
 </template>
 
 <style scoped>
-
-footer {
-  position: fixed;
-  bottom: 0;
-  background-color: #d9d9d9;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 30px 0;
-}
 
 </style>
