58b4a835f0fbcee0ca52b04e2970d609a84ae79d
ReehLimas <reehlimas08@gmail.com>
2024-08-28 11:55:56 -0300

    feat: página de modelos alterada #9

--- src/views/ModelosView.vue
+++ src/views/ModelosView.vue
@@ -2,14 +2,31 @@
 </script>
 
 <template>
-  <div>
-    <img class="imagem" src="" alt="imagem1">
+  <div class="models-products">
+    <img src="@/img/portaodezesete.jpg" alt="imagem1" >
+    <p>R$1000</p>
+    <p>Portão branco de alumínio</p>
   </div>
+  <div class="models-products">
+    <img src="@/img/portaoquinze.jpg" alt="" >
+    <p>R$1000</p>
+    <p>Portão alto de alumínio branco</p>
+  </div>
+ 
+ 
 </template>
 
 <style scoped>
-.imagem {
-
+img{
+  height: 400px;
 }
+.models-products{
+  display: grid;
+ padding: 1rem 1rem;
+  gap: 2px;
+  justify-content: center;
+  text-align: center;
+}
+
 
 </style>
