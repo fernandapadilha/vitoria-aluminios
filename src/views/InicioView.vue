b0c69e8796188b99a4f752add03e17fdeca57bf0
julianeblasius <juliblasius5@gmail.com>
2024-08-28 11:50:14 -0300

    feat: melhorias na página inicial #8

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -3,7 +3,28 @@
 </script>
 
 <template>
-  <main>
-    
-  </main>
+  <div class="slide">
+    <img src="@/img/Portao1.png" alt="">
+  </div>
+
+  <div class="description">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aliquam incidunt hic maxime, aut dolores magni accusamus veniam ipsum cumque tenetur repellendus ea et fugiat quis ipsam exercitationem soluta. Perspiciatis.</p>
+  </div>
 </template>
+
+<style scoped>
+
+  .slide img {
+    display: grid;
+    grid-template-columns: auto ;
+    width: 30%;
+    height: 40%;
+    margin-bottom: 30px;
+    margin-left: 100px;
+  }
+
+  .description p {
+    width: 50%;
+
+  }
+</style>
