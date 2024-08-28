5071aaf7a3bc4c5335d30eb18be199e233444d6e
bernardesraischruama <bernardesraichruama@gmail.com>
2024-08-28 09:30:04 -0300

    feat: pasta imagens #9

--- src/views/ModelosView.vue
+++ src/views/ModelosView.vue
@@ -1,9 +1,15 @@
-<script setup></script>
+<script setups>
+</script>
 
 <template>
-  
+  <div>
+    <img class="imagem" src="" alt="imagem1">
+  </div>
 </template>
 
 <style scoped>
+.imagem {
+
+}
 
 </style>
