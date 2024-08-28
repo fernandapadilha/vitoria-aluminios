956a20ab228a9f19286d70a43f35d11f89a3ac56
fernandapadilha <vitoriafernandaifc@gmail.com>
2024-08-28 11:54:45 -0300

    Feat: renderização lista palito #10

--- src/views/OrcamentoView.vue
+++ src/views/OrcamentoView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { ref } from 'vue';
 
 const acessorios = ref([
     {
@@ -25,30 +25,48 @@ const acessorios = ref([
     }
 ]);
 
-const palito = ref([
+const palitos = ref([
     {   
         id: 1, 
         nome: 'Redondo', 
         preco: 60, 
-        descricao: '',
-        img: ''},
+        descricao: 'ghfffyu',
+        img: '../img/tubo-redondo-branco.png',
+        alt: ''
+    },
     {  
         id: 2, 
         nome: 'Quadrado', 
         preco: 49, 
-        descricao: '',
-        img: ''
+        descricao: 'tgyfytgt',
+        img: '../img/tubo-quadrado-branco.png',
+        alt: ''
     }
 ]);
 
-const cor = ref([
+const cores = ref([
     {
         id: 1,
         nome: 'Branco',
-        preco: 3000,
-        img: ''
+        img: '',
+        alt: ''
+    },
+    {
+        id: 2,
+        nome: 'Preto',
+        img: '',
+        alt: ''
     }
 ]);
+
+const palitoSelecionado = ref({
+    id: "", 
+    nome: "", 
+    preco: 0, 
+    descricao: "", 
+    img: "",
+    alt: '',
+})
 
 </script>
 
@@ -66,7 +84,45 @@ const cor = ref([
                 <h5>Tipo do palito:</h5>
                 <section>
                     <div>
-                        <select name="palito" id="palito" v-model="palito">
+                        <select name="palitoS" id="palitoS" v-model="palitoSelecionado">
+                            <option v-for="palito in palitos" :key="palito.id" :value="palito">{{ palito.nome }}</option>
+                        </select>
+                        <div v-if="palitoSelecionado.descricao">
+                            <p>{{ palitoSelecionado.descricao }}</p>
+                            <p>Valor: {{ palitoSelecionado.preco }}</p>
+                        </div> 
+                    </div>
+                   
+                    <div v-if="palitoSelecionado.img">
+                        <img src="{{ palitoSelecionado.img }}" alt="{{ palitoSelecionado.alt }}">
+                    </div>
+                </section>
+            </article>
+
+            <article>
+                <h5>Tipo do palito:</h5>
+                <section>
+                    <div>
+                        <select name="cores" id="cores" v-model="cores">
+                            
+                        </select>
+                        <div>
+                            <p>Informações adicionais</p>
+                            <p>Valor: uhfdslk</p>
+                        </div> 
+                    </div>
+                   
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                </section>
+            </article>
+
+            <article>
+                <h5>Tipo do palito:</h5>
+                <section>
+                    <div>
+                        <select name="acessorios" id="acessorios" v-model="acessorios">
                             
                         </select>
                         <div>
@@ -85,4 +141,8 @@ const cor = ref([
     
 </template>
 
-<style scoped></style>
+<style scoped>
+
+    
+
+</style>
