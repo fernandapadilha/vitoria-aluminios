1d75c1d8952b5aa8cf94b256943f9ee5d99f5d63
bernardesraischruama <bernardesraichruama@gmail.com>
2024-08-31 11:07:18 -0300

    Feat: valores models-products #9

--- src/views/ModelosView.vue
+++ src/views/ModelosView.vue
@@ -1,32 +1,62 @@
-<script setups>
-</script>
+<script setups></script>
 
 <template>
-  <div class="models-products">
-    <img src="@/img/portaodezesete.jpg" alt="imagem1" >
-    <p>R$1000</p>
-    <p>Portão branco de alumínio</p>
+  <div class="grid-models">
+    <div class="models-products">
+      <img src="@/img/portaodezesete.jpg" alt="imagem1" />
+      <p>Perfil Estrutural do Portão branco de Alumínio</p>
+      <p class="p">R$1500,00</p>
+    </div>
+    <div class="models-products">
+      <img src="@/img/portaoquinze.jpg" alt="" />
+      <p>Perfil Estrutural do Portão branco de Alumínio</p>
+      <p class="p">R$1550,00</p>
+    </div>
+    <div class="models-products">
+      <img src="@/img/portaodezoito.jpg" alt="" />
+      <P>Perfil Estrutural do Portão branco de Alumínio</P>
+      <P class="p">R$1000,00</P>
+    </div>
+    <div class="models-products">
+      <img src="@/img/portaodois.jpg" alt="" />
+      <p>Perfil Estrutural do Portão preto de Alumínio</p>
+      <p class="p">R$1000,00</p>
+      <avaliacao>ícone</avaliacao>
+    </div>
   </div>
-  <div class="models-products">
-    <img src="@/img/portaoquinze.jpg" alt="" >
-    <p>R$1000</p>
-    <p>Portão alto de alumínio branco</p>
-  </div>
- 
- 
+
+
 </template>
 
 <style scoped>
-img{
-  height: 400px;
+img {
+  height: 350px;
+  width: 370px;
+  border-radius: 10px;
 }
-.models-products{
-  display: grid;
- padding: 1rem 1rem;
+.models-products {
   gap: 2px;
-  justify-content: center;
   text-align: center;
-}
+  margin: 20px;
+  padding: 10px;
+  border: 1px solid rgb(221, 221, 221);
+  border-radius: 4%;
+  font-family: "Barlow Condensed", sans-serif;
+  font-weight: 500;
+  font-style: italic;
 
+}
+.grid-models {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  align-items: center;
+  margin: 5%;
+ 
+}
+.p {
+  font-family: "Barlow Condensed", sans-serif;
+  font-weight: 800;
+  font-style: italic;
+}
 
 </style>
