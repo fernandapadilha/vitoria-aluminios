518d673a10f86cef9086ca55e819d30420f6d148
fernandapadilha <vitoriafernandaifc@gmail.com>
2024-10-09 11:51:39 -0300

    Feat: Pinia adicionado #30

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
+
 import './assets/main.css'
 import('https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
+
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia();
 const app = createApp(App)
 
-app.use(router)
 
+app.use(pinia)
+app.use(router)
 app.mount('#app')
